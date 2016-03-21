69d80db3bbc76b711c32d0f81292753f7787aa3f
ARIFF <ariffsyed10@gmail.com>
2016-03-21 21:51:03 +0800

    Almost ready

---
+++ TutorEditViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TutorEditViewController.swift
+//  iTutor
+//
+//  Created by Sharifah Nazreen Ashraff ali on 18/03/2016.
+//  Copyright © 2016 Syed Mohamed Ariff. All rights reserved.
+//
+
+import Foundation
---
+++ iTutor/TutorTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TutorTableViewCell.swift
+//  iTutor
+//
+//  Created by Sharifah Nazreen Ashraff ali on 08/03/2016.
+//  Copyright © 2016 Syed Mohamed Ariff. All rights reserved.
+//
+
+import UIKit
+
+class TutorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
