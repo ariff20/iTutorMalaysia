4b7049a9ae5142e7d26d991a4322a90d0a501ea5
ARIFF <ariffsyed10@gmail.com>
2016-04-12 16:17:50 +0800

    Added email search

---
+++ iTutor/BestTutorsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BestTutorsTableViewCell.swift
+//  iTutor
+//
+//  Created by Sharifah Nazreen Ashraff ali on 09/04/2016.
+//  Copyright © 2016 Syed Mohamed Ariff. All rights reserved.
+//
+
+import UIKit
+
+class BestTutorsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iTutor/ForgotYourPasswordViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ForgotYourPasswordViewController.swift
+//  iTutor
+//
+//  Created by Sharifah Nazreen Ashraff ali on 01/04/2016.
+//  Copyright © 2016 Syed Mohamed Ariff. All rights reserved.
+//
+
+import Foundation
